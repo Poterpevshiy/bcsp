57ecd24cbe6a7fea91935255f9ff553ddbd774e7
ygrumov <ygrumov@URA>
2021-07-08 18:06:39 +0300

    Add project files.

---
+++ bcsp/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace bcsp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PathArc1TextBox = new System.Windows.Forms.TextBox();
+            this.PathArc2TextBox = new System.Windows.Forms.TextBox();
+            this.Path1button = new System.Windows.Forms.Button();
+            this.Path2button = new System.Windows.Forms.Button();
+            this.res_textBox = new System.Windows.Forms.TextBox();
+            this.Startbutton = new System.Windows.Forms.Button();
+            this.openPath1button = new System.Windows.Forms.Button();
+            this.openPath2button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PathArc1TextBox
+            // 
+            this.PathArc1TextBox.Location = new System.Drawing.Point(173, 30);
+            this.PathArc1TextBox.Multiline = true;
+            this.PathArc1TextBox.Name = "PathArc1TextBox";
+            this.PathArc1TextBox.Size = new System.Drawing.Size(878, 28);
+            this.PathArc1TextBox.TabIndex = 4;
+            this.PathArc1TextBox.TextChanged += new System.EventHandler(this.PathArc1TextBox_TextChanged);
+            // 
+            // PathArc2TextBox
+            // 
+            this.PathArc2TextBox.Location = new System.Drawing.Point(173, 107);
+            this.PathArc2TextBox.Multiline = true;
+            this.PathArc2TextBox.Name = "PathArc2TextBox";
+            this.PathArc2TextBox.Size = new System.Drawing.Size(878, 28);
+            this.PathArc2TextBox.TabIndex = 5;
+            this.PathArc2TextBox.TextChanged += new System.EventHandler(this.PathArc2TextBox_TextChanged);
+            // 
+            // Path1button
+            // 
+            this.Path1button.Location = new System.Drawing.Point(29, 11);
+            this.Path1button.Name = "Path1button";
+            this.Path1button.Size = new System.Drawing.Size(103, 58);
+            this.Path1button.TabIndex = 1;
+            this.Path1button.Text = "Select the folder";
+            this.Path1button.UseVisualStyleBackColor = true;
+            this.Path1button.Click += new System.EventHandler(this.Path1button_Click);
+            // 
+            // Path2button
+            // 
+            this.Path2button.Location = new System.Drawing.Point(29, 88);
+            this.Path2button.Name = "Path2button";
+            this.Path2button.Size = new System.Drawing.Size(103, 58);
+            this.Path2button.TabIndex = 2;
+            this.Path2button.Text = "Select the folder";
+            this.Path2button.UseVisualStyleBackColor = true;
+            this.Path2button.Click += new System.EventHandler(this.Path2button_Click);
+            // 
+            // res_textBox
+            // 
+            this.res_textBox.Location = new System.Drawing.Point(29, 237);
+            this.res_textBox.Multiline = true;
+            this.res_textBox.Name = "res_textBox";
+            this.res_textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.res_textBox.Size = new System.Drawing.Size(1022, 201);
+            this.res_textBox.TabIndex = 6;
+            // 
+            // Startbutton
+            // 
+            this.Startbutton.Location = new System.Drawing.Point(29, 166);
+            this.Startbutton.Name = "Startbutton";
+            this.Startbutton.Size = new System.Drawing.Size(103, 58);
+            this.Startbutton.TabIndex = 3;
+            this.Startbutton.Text = "Start";
+            this.Startbutton.UseVisualStyleBackColor = true;
+            this.Startbutton.Click += new System.EventHandler(this.Startbutton_Click);
+            // 
+            // openPath1button
+            // 
+            this.openPath1button.Font = new System.Drawing.Font("Segoe UI", 3.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.openPath1button.Location = new System.Drawing.Point(131, 11);
+            this.openPath1button.Name = "openPath1button";
+            this.openPath1button.Size = new System.Drawing.Size(12, 58);
+            this.openPath1button.TabIndex = 7;
+            this.openPath1button.Text = "O\r\np\r\ne\r\nn";
+            this.openPath1button.UseVisualStyleBackColor = true;
+            this.openPath1button.Click += new System.EventHandler(this.openPath1button_Click);
+            // 
+            // openPath2button
+            // 
+            this.openPath2button.Font = new System.Drawing.Font("Segoe UI", 3.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.openPath2button.Location = new System.Drawing.Point(131, 88);
+            this.openPath2button.Name = "openPath2button";
+            this.openPath2button.Size = new System.Drawing.Size(12, 58);
+            this.openPath2button.TabIndex = 8;
+            this.openPath2button.Text = "Open";
+            this.openPath2button.UseVisualStyleBackColor = true;
+            this.openPath2button.Click += new System.EventHandler(this.openPath2button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 3.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(131, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(12, 58);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.openTXTbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.openPath2button);
+            this.Controls.Add(this.openPath1button);
+            this.Controls.Add(this.Startbutton);
+            this.Controls.Add(this.res_textBox);
+            this.Controls.Add(this.Path2button);
+            this.Controls.Add(this.Path1button);
+            this.Controls.Add(this.PathArc2TextBox);
+            this.Controls.Add(this.PathArc1TextBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ololo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox PathArc1TextBox;
+        private System.Windows.Forms.TextBox PathArc2TextBox;
+        private System.Windows.Forms.Button Path1button;
+        private System.Windows.Forms.Button Path2button;
+        private System.Windows.Forms.Button Startbutton;
+        public System.Windows.Forms.TextBox res_textBox;
+        private System.Windows.Forms.Button openPath1button;
+        private System.Windows.Forms.Button openPath2button;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ bcsp/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+
+namespace bcsp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Startbutton.Enabled = false;
+        }
+        string dirName, dirName2;
+
+        private void Path1button_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog DirDialog = new FolderBrowserDialog();
+            DirDialog.Description = "Выбор директории1";
+            DirDialog.SelectedPath = @"C:\";
+
+            if (DirDialog.ShowDialog() == DialogResult.OK)
+            {
+                PathArc1TextBox.Text = DirDialog.SelectedPath;
+            }
+        }
+
+        private void Path2button_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog DirDialog = new FolderBrowserDialog();
+            DirDialog.Description = "Выбор директории2";
+            DirDialog.SelectedPath = @"C:\";
+
+            if (DirDialog.ShowDialog() == DialogResult.OK)
+            {
+                PathArc2TextBox.Text = DirDialog.SelectedPath;
+            }
+        }
+
+        private void Startbutton_Click(object sender, EventArgs e)
+        {            
+            dirName = PathArc1TextBox.Text;
+
+            dirName2 = PathArc2TextBox.Text;
+
+            if (PathArc1TextBox.Text == PathArc2TextBox.Text)
+            {
+                MessageBox.Show(
+    "Каталоги разные быть должны",
+    "Сообщение",
+    MessageBoxButtons.OK,
+    MessageBoxIcon.Information,
+    MessageBoxDefaultButton.Button1);                
+            } else {
+                res_textBox.Text = "";
+
+                using (TextWriter text = new StreamWriter(dirName + @"\bcsp.txt"))
+                {
+                    GetFiles(dirName, dirName2, text);
+                }
+
+   //             if (res_textBox.Text != "")
+     //           {
+                    res_textBox.Text += (Environment.NewLine + "complete");
+         //       }
+           //     else
+             //   {
+               //     res_textBox.Text = ("нет одинаковых файлов");
+              //  }
+            }
+        }
+
+        private static string ComputeMD5Checksum(string path)
+        {
+            using (FileStream fs = System.IO.File.OpenRead(path))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] fileData = new byte[fs.Length];
+                fs.Read(fileData, 0, (int)fs.Length);
+                byte[] checkSum = md5.ComputeHash(fileData);
+                string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                return result;
+            }
+        }
+
+        private void GetFiles(string path1, string path2, TextWriter text)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path1);
+                DirectoryInfo dir2 = new DirectoryInfo(path2);
+                FileInfo[] files = dir.GetFiles();
+                FileInfo[] files2 = dir2.GetFiles();
+                foreach (FileInfo f in files)
+                {
+                    if (f.Name == "bcsp.txt")
+                    {
+                        continue;
+                    }
+                    foreach (FileInfo f2 in files2)
+                    {
+                        if (ComputeMD5Checksum(f2.FullName) == ComputeMD5Checksum(f.FullName))
+                        {
+                            text.WriteLine(f.Name + " - " + f.LastWriteTime);
+                            text.WriteLine(f2.Name + " - " + f2.LastWriteTime);
+                            text.WriteLine("\t" + "одинаковые - " + ComputeMD5Checksum(f.FullName) + "\n");
+                            res_textBox.Text += (f.Name + " = " + f2.Name + " = " + ComputeMD5Checksum(f.FullName) + Environment.NewLine);
+                        }
+                    }
+
+                }
+
+                foreach (DirectoryInfo d in dir.GetDirectories())
+                {
+                    GetFiles(path1 + d.Name + @"\", path2 + d.Name + @"\", text);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void openPath1button_Click(object sender, EventArgs e)
+        {
+            //            if (Directory.Exists(PathArc1TextBox.Text))
+            //            {
+            //                Process.Start(PathArc1TextBox.Text);
+            //            }
+            Nextver();
+        }
+
+        private void PathArc1TextBox_TextChanged(object sender, EventArgs e)
+        {
+            StartbuttonEnabled();
+        }
+
+        private void openTXTbutton_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(dirName + @"\bcsp.txt"))
+            {
+                Process.Start(@"notepad.exe ", dirName + @"\bcsp.txt");
+            }
+        }
+
+        private void PathArc2TextBox_TextChanged(object sender, EventArgs e)
+        {
+            StartbuttonEnabled();
+        }
+
+        private void StartbuttonEnabled()
+        {
+            if (Directory.Exists(PathArc1TextBox.Text) && Directory.Exists(PathArc2TextBox.Text))
+            {
+                Startbutton.Enabled = true;
+            }
+            else
+            {
+                Startbutton.Enabled = false;
+            }
+        }
+
+        private void openPath2button_Click(object sender, EventArgs e)
+        {
+            Nextver();
+        }
+
+        private void Nextver()
+        {
+            MessageBox.Show(
+            "Кнопка пока не работает",
+            "Ожидайте следующую версию",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Information,
+            MessageBoxDefaultButton.Button1);
+        }
+    }
+}
